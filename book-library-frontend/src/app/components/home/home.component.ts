312866849d417b641f89e3adf9d13e48e7735265
alexia <alexiamilanesio9@gmail.com>
2023-05-26 15:59:37 +0200

    added functionalities to home component

--- book-library-frontend/src/app/components/home/home.component.ts
+++ book-library-frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/models';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -8,16 +9,29 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  isLoggedIn: boolean;
+  isLoggedIn!: boolean;
+  currentUser!: User;
 
+  
   constructor(private userService: UserService, private router: Router) {
     this.isLoggedIn = this.userService.getCurrentUser('currentUser').isLoggedIn;
+    this.currentUser = this.userService.currentUser;
   }
 
   ngOnInit(): void {}
 
 
-  goToLogin(): void {
+  public goToLogin(): void {
     this.router.navigate(['Login']);
+  }
+  
+
+  public goToLibraries(): void {
+    this.router.navigate(['Libraries']);
+  }
+
+
+  public goToProfile(): void {
+    this.router.navigate([`Profile/${this.currentUser.id}`]);
   }
 }
