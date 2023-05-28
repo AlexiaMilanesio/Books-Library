eed6ef3fda59e6db4af18ae2e52103f2f49084f5
alexia <alexiamilanesio9@gmail.com>
2023-05-28 12:32:35 +0200

    created user class, and updated packages

--- book-library-backend/books-library.Api/Classes/BookComplete.cs
+++ book-library-backend/books-library.Api/Classes/BookComplete.cs
@@ -9,10 +9,8 @@ namespace books_library.Api.Wrappers
         public int year { get; set; }
         public string publisher { get; set; }
         public string image_url { get; set; }
-        //public string library { get; set; }
         public int libraryId { get; set; }
         public string author { get; set; }
-        //public int authorId { get; set; }
     }
 }
 
---
+++ book-library-backend/books-library.Api/Classes/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace books_library.Api.Classes
+{
+	public class User
+	{
+		public string Id { get; set; }
+        public bool IsSuperAdmin { get; set; }
+        public bool IsLoggedIn { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        //public User(string id, bool isSuperAdmin, bool isLoggedIn, string name, string lastname, string email, string password)
+        //{
+        //    Id = id;
+        //    IsSuperAdmin = isSuperAdmin;
+        //    IsLoggedIn = isLoggedIn;
+        //    Name = name;
+        //    Lastname = lastname;
+        //    Email = email;
+        //    Password = password;
+        //}
+    }
+}
+
